170d6b77fdc38e1e5320baa43663c5040e344895
arslanmatloob <raoarslan263@gmail.com>
2025-05-26 12:26:43 +0500

    project setup

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup>
+const { $wow } = useNuxtApp();
+
+onMounted(() => {
+  $wow.init();
+})
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
+  </div>
+</template>
